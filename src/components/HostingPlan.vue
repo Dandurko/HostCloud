02967c324555fc95fc0dc00a9bc3fc2f95bc78df
dandur <deniscicoo@gmail.com>
2024-01-09 16:52:19 +0100

    Components

---
+++ src/components/HostingPlan.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <!-- Pricing Starts Here -->
+
+
+
+             <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="pricing-item">
+              <h4>Advanced Cloud</h4>
+              <div class="price">
+                <h6>$72.25</h6>
+                <span>monthly</span>
+              </div>
+              <p>Etiam sit amet placerat lacus, sed placerat mauris. Vestibulum malesuada vehicula sapien non tempus.</p>
+              <div class="dev"></div>
+              <ul>
+                <li><i class="fa fa-check"></i>4 TB Cloud Space</li>
+                <li><i class="fa fa-check"></i>20 TB Data Transfer</li>
+                <li><i class="fa fa-check"></i>Fully Managed Panel</li>
+                <li><i class="fa fa-check"></i>15-minute Quick Support</li>
+                <li><i class="fa fa-check"></i>Top Notch Web Apps</li>
+                <li><i class="fa fa-check"></i>Advanced Scalable</li>
+              </ul>
+              <a href="#" class="main-button">Take it now</a>
+            </div>
+          </div>
+  
+    <!-- Pricing Ends Here -->
+</template>
---
+++ src/components/Service.vue
@@ -0,0 +1,9 @@
+<template>
+       <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="service-item">
+              <i class="fa fa-cloud"></i>
+              <h4>VPS Cloud</h4>
+              <p>Fusce blandit commodo metus eu suscipit. Nullam ut blandit neque. Aenean nec tellus ac velit elementum tempor. Pellentesque nec urna ipsum.</p>
+            </div>
+          </div>
+</template>
