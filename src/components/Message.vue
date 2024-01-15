a84f7eae09e5dbe0dabaae7b8d2be5bde5a97cad
dandur <deniscicoo@gmail.com>
2024-01-15 15:09:39 +0100

    Components

---
+++ src/components/Message.vue
@@ -0,0 +1,59 @@
+<template>
+  <div v-if="show" class="message">
+    {{ message }}
+    <div class="close" @click="closeMessage">Close</div>
+  </div>
+</template>
+
+<script>
+//pokial zostane cas tak hodit to aj do contact us
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'info',
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      show: true,
+    };
+  },
+  methods: {
+    closeMessage() {
+      this.show = false;
+      this.$emit('closeMessage'); // Emitovat událost closeMessage
+    },
+  },
+};
+</script>
+
+
+  <style scoped>
+.message {
+  position:absolute;
+  bottom:100px;
+  margin-top: 20px;
+  padding: 15px;
+  border: 1px solid #d6d6d6;
+  background-color: #d9edf7;
+  border-radius: 4px;
+  height:100pxpx;
+  text-align: center;
+  width: 33.3%;
+}
+.close {
+
+  color:white;
+  font-size: 20px;
+  background-color: green; /* Change the color as needed */
+  cursor: pointer;
+}
+
+/* Add more styles for other alert types as needed */
+</style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,246 +1,251 @@
 <template>
    
 
-
-
   <div class="banner">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 offset-md-2">
-            <div class="header-text caption">
-              <h2>Search your domain</h2>
-              <div id="search-section">
-              	<form id="suggestion_form" name="gs" method="get" action="#">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div class="header-text caption">
+            <h2>Search your domain</h2>
+            <div id="search-section">
+
+              <div>
+    <div v-if="showMessage">
+      <Message :message="message" @closeMessage="closeMessage" />
+    </div>
+
+    <form
+      id="suggestion_form"
+      name="gs"
+      method="get"
+      action="#"
+      @submit.prevent="submitForm"
+    >
+    >
                 <div class="searchText">
-                  
-                  <input type="text" name="q" class="searchText" placeholder="Enter your domain here..." autocomplete="on">
+                  <input
+                    v-model="domain"
+                    type="text"
+                    name="q"
+                    class="searchText"
+                    placeholder="Enter your domain here..."
+                    autocomplete="on"
+                  />
                   <ul>
-                    <li><label><input type="checkbox" name="ext_com" value="1"><span>.com <em>($10/yr)</em></span></label></li>
-                    <li><label><input type="checkbox" name="ext_net" value="1"><span>.net <em>($12/yr)</em></span></label></li>
-                    <li><label><input type="checkbox" name="ext_org" value="1"><span>.org <em>($8/yr)</em></span></label></li>
-                    <li><label><input type="checkbox" name="ext_in" value="1"><span>.in <em>($6/yr)</em></span></label></li>
+                    <li>
+                      <label
+                        ><input type="checkbox" name="ext_com" value="1" /><span
+                          >.com <em>($10/yr)</em></span
+                        ></label
+                      >
+                    </li>
+                    <li>
+                      <label
+                        ><input type="checkbox" name="ext_net" value="1" /><span
+                          >.net <em>($12/yr)</em></span
+                        ></label
+                      >
+                    </li>
+                    <li>
+                      <label
+                        ><input type="checkbox" name="ext_org" value="1" /><span
+                          >.org <em>($8/yr)</em></span
+                        ></label
+                      >
+                    </li>
+                    <li>
+                      <label
+                        ><input type="checkbox" name="ext_in" value="1" /><span
+                          >.in <em>($6/yr)</em></span
+                        ></label
+                      >
+                    </li>
                   </ul>
                 </div>
-                    <input type="submit" name="results" class="main-button" value="Search Now">
-                 </form>
-               <div class="advSearch_chkbox">
-               </div>
-              </div>
+
+      <input
+        type="submit"
+        name="results"
+        class="main-button"
+        value="Search Now"
+      />
+    </form>
+  </div>
+
+
+
+              <div class="advSearch_chkbox"></div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- Banner Ends Here -->
+  </div>
+  <!-- Banner Ends Here -->
 
-    <!-- Trusted Starts Here -->
-    <div class="trusted-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="trusted-section-heading">
-              <h4>TRUSTED BY 1,250+ HAPPY CUSTOMERS WORLDWIDE</h4>
-            </div>
+  <!-- Trusted Starts Here -->
+  <div class="trusted-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="trusted-section-heading">
+            <h4>TRUSTED BY 1,250+ HAPPY CUSTOMERS WORLDWIDE</h4>
           </div>
-          <div class="col-md-12">
-            <div class="owl-trusted owl-carousel">
-              <div class="trusted-item">
-                <img src="src/assets/images/trusted-01.png" alt="trusted 1">
-              </div>
-              <div class="trusted-item">
-                <img src="src/assets/images/trusted-01.png" alt="trusted 2">
-              </div>
-              <div class="trusted-item">
-                <img src="src/assets/images/trusted-01.png" alt="trusted 3">
-              </div>
-              <div class="trusted-item">
-                <img src="src/assets/images/trusted-01.png" alt="trusted 4">
-              </div>
-              <div class="trusted-item">
-                <img src="src/assets/images/trusted-01.png" alt="trusted 5">
-              </div>
-              <div class="trusted-item">
-                <img src="src/assets/images/trusted-01.png" alt="trusted 6">
-              </div>
-              <div class="trusted-item">
-                <img src="src/assets/images/trusted-01.png" alt="trusted 7">
-              </div>
+        </div>
+        <div class="col-md-12">
+          <div class="owl-trusted owl-carousel">
+            <div class="trusted-item">
+              <img src="src/assets/images/trusted-01.png" alt="trusted 1" />
+            </div>
+            <div class="trusted-item">
+              <img src="src/assets/images/trusted-01.png" alt="trusted 2" />
+            </div>
+            <div class="trusted-item">
+              <img src="src/assets/images/trusted-01.png" alt="trusted 3" />
+            </div>
+            <div class="trusted-item">
+              <img src="src/assets/images/trusted-01.png" alt="trusted 4" />
+            </div>
+            <div class="trusted-item">
+              <img src="src/assets/images/trusted-01.png" alt="trusted 5" />
+            </div>
+            <div class="trusted-item">
+              <img src="src/assets/images/trusted-01.png" alt="trusted 6" />
+            </div>
+            <div class="trusted-item">
+              <img src="src/assets/images/trusted-01.png" alt="trusted 7" />
             </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- Trusted Ends Here -->
+  </div>
+  <!-- Trusted Ends Here -->
 
-
-    <!-- Services Starts Here -->
-    <div class="services-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <span>Hosting Services</span>
-              <h2>Services we provide</h2>
-              <p>Host Cloud is a professional Bootstrap 4 template by TemplateMo for your hosting company websites. There are 4 HTML pages included in this template. You can feel free to customize anything. Please share this template to your friends. Thank you.</p>
-            </div>
+  <div class="pricing-section">
+    <div class="background-image-pricing"></div>
+    <div class="container">
+      <div class="row">
+        <HostingPlan />
+      </div>
+    </div>
+  </div>
+  <!-- Features Starts Here -->
+  <div class="features-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <span>Best Quality for you</span>
+            <h2>Cloud Features</h2>
           </div>
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="service-item">
-              <i class="fa fa-database"></i>
-              <h4>10x Light Cloud</h4>
-              <p>Aenean sit amet leo vitae tellus vehicula tincidunt vel sed lorem. Nullam tincidunt commodo magna, id aliquam sapien sollicitudin id.</p>
+        </div>
+        <div class="col-md-6">
+          <div class="feature-item">
+            <div class="icon">
+              <img src="src/assets/images/feature-01.png" alt="" />
             </div>
+            <h4>Load Balanced</h4>
+            <p>
+              Nulla nisl ex, vehicula in urna nec, commodo consectetur augue. Vivamus nec
+              metus mauris. Praesent lacinia tempus urna.
+            </p>
           </div>
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="service-item">
-              <i class="fa fa-database"></i>
-              <h4>Managed VPS Cloud</h4>
-              <p>You are not allowed to re-distribute this template as a downloadable ZIP file on any template collection website. Please <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact us</a> if you want to.</p>
+        </div>
+        <div class="col-md-6">
+          <div class="feature-item">
+            <div class="icon">
+              <img src="src/assets/images/feature-01.png" alt="" />
             </div>
+            <h4>99.9% Uptime Guaranteed</h4>
+            <p>
+              Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas
+              en pin sustainable carry scenester.
+            </p>
           </div>
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="service-item">
-              <i class="fa fa-database"></i>
-              <h4>Fully Dedicated</h4>
-              <p>Aenean sit amet leo vitae tellus vehicula tincidunt vel sed lorem. Nullam tincidunt commodo magna, id aliquam sapien sollicitudin id.</p>
+        </div>
+        <div class="col-md-6">
+          <div class="feature-item">
+            <div class="icon">
+              <img src="src/assets/images/feature-01.png" alt="" />
             </div>
+            <h4>Top Reliability</h4>
+            <p>
+              Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas
+              en pin sustainable carry scenester.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="feature-item">
+            <div class="icon">
+              <img src="src/assets/images/feature-01.png" alt="" />
+            </div>
+            <h4>High Security</h4>
+            <p>
+              Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas
+              en pin sustainable carry scenester.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="feature-item">
+            <div class="icon">
+              <img src="src/assets/images/feature-01.png" alt="" />
+            </div>
+            <h4>Quality Hardwares</h4>
+            <p>
+              Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas
+              en pin sustainable carry scenester.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="feature-item">
+            <div class="icon">
+              <img src="src/assets/images/feature-01.png" alt="" />
+            </div>
+            <h4>Network Solutions</h4>
+            <p>
+              Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas
+              en pin sustainable carry scenester.
+            </p>
           </div>
         </div>
       </div>
     </div>
-    <!-- Services Ends Here -->
-    <div class="pricing-section">
-      <div class="background-image-pricing">
-      </div>
-      <div class="container">
-        <div class="row">
-        
-    <HostingPlan/>
-
-        </div>
-      </div>
-    </div>
-    <!-- Features Starts Here -->
-    <div class="features-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <span>Best Quality for you</span>
-              <h2>Cloud Features</h2>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-item">
-              <div class="icon">
-                <img src="src/assets/images/feature-01.png" alt="">
-              </div>
-              <h4>Load Balanced</h4>
-              <p>Nulla nisl ex, vehicula in urna nec, commodo consectetur augue. Vivamus nec metus mauris. Praesent lacinia tempus urna.</p>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-item">
-              <div class="icon">
-                <img src="src/assets/images/feature-01.png" alt="">
-              </div>
-              <h4>99.9% Uptime Guaranteed</h4>
-              <p>Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas en pin sustainable carry scenester.</p>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-item">
-              <div class="icon">
-                <img src="src/assets/images/feature-01.png" alt="">
-              </div>
-              <h4>Top Reliability</h4>
-              <p>Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas en pin sustainable carry scenester.</p>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-item">
-              <div class="icon">
-                <img src="src/assets/images/feature-01.png" alt="">
-              </div>
-              <h4>High Security</h4>
-              <p>Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas en pin sustainable carry scenester.</p>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-item">
-              <div class="icon">
-                <img src="src/assets/images/feature-01.png" alt="">
-              </div>
-              <h4>Quality Hardwares</h4>
-              <p>Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas en pin sustainable carry scenester.</p>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-item">
-              <div class="icon">
-                <img src="src/assets/images/feature-01.png" alt="">
-              </div>
-              <h4>Network Solutions</h4>
-              <p>Lorem ipsum dolor ame taxidermy sriracha cardigan salvia actually vice migas en pin sustainable carry scenester.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Features Ends Here -->
-
-
-    <!-- Testimonials Starts Here -->
-    <div class="testimonials-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <span>Testimonials</span>
-              <h2>What they say about us</h2>
-            </div>
-          </div>
-          <div class="col-md-10 offset-md-1">
-            <div class="owl-testimonials owl-carousel">
-              <div class="testimonial-item">
-                <div class="icon">
-                  <i class="fa fa-quote-right"></i>
-                </div>
-                <p>"Ut elementum a elit sed tristique. Pellentesque sed semper erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean quam erat, rutrum ut malesuada."</p>
-                <h4>William Smith</h4>
-                <span>New Co-Founder</span>
-              </div>
-              <div class="testimonial-item">
-                <div class="icon">
-                  <i class="fa fa-quote-right"></i>
-                </div>
-                <p>"Quisque lacinia sed velit et maximus. Quisque dictum, lacus a malesuada finibus, arcu magna luctus risus, eu accumsan risus elit vitae lacus."</p>
-                <h4>Vinny Smart</h4>
-                <span>Digital Marketer</span>
-              </div>
-              <div class="testimonial-item">
-                <div class="icon">
-                  <i class="fa fa-quote-right"></i>
-                </div>
-                <p>"Vestibulum mauris ipsum, tempor tincidunt justo sit amet, bibendum tincidunt dui. Aenean molestie, odio quis viverra ultricies, leo tellus lacinia neque, sit amet maximus tortor nunc aliquet felis."</p>
-                <h4>Trevor Liam</h4>
-                <span>Technology Chef</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Testimonials Ends Here -->
-
-
-  
+  </div>
+  <!-- Features Ends Here -->
 </template>
 <script>
+import Message from "@/components/Message.vue";
 import HostingPlan from "@/components/HostingPlan.vue";
 export default {
   components: {
-    HostingPlan, 
+    Message,
+    HostingPlan
+  },
+  data() {
+    return {
+      showMessage: false,
+      message: "",
+      domain: "", // New data property for the input value
+    };
+  },
+  methods: {
+    submitForm() {
+      // Check if the domain input is empty
+      if (this.domain.trim() === "") {
+        this.showMessage = true;
+        this.message = "Please enter a domain.";
+      } else {
+        this.showMessage = true;
+        this.message = "Nice";
+      }
+    },
+    closeMessage() {
+      this.showMessage = false; // Aktualizovat stav showMessage na false
+    },
   },
 };
 </script>
